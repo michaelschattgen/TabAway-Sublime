400249feb9796b1be5fc74d7e2c5b9bd706a1fd8
Reverp <contact@reverbs.pw>
2016-01-26 22:22:42 +0100

    Started working on number of windows

--- TabAway.py
+++ TabAway.py
@@ -5,5 +5,12 @@ import time
 
 
 class TabAwayCommand(sublime_plugin.WindowCommand):
-
     def run(self):
+    	active = self.window.active_view();
+    	print("ok")
+    	for i in range(0, self.window.num_groups()):
+    		print(i)
+
+class TabAwayListener(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+        view.window().run_command("tab_away")
