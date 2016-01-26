1090171ec4069e0121d9689445998cda7d6808c0
Reverp <contact@reverbs.pw>
2016-01-27 00:22:48 +0100

    Added settings

--- TabAway.py
+++ TabAway.py
@@ -1,18 +1,28 @@
 import sublime_plugin
+import sublime
 
 
 class TabAwayCommand(sublime_plugin.WindowCommand):
     def run(self):
+        settings = sublime.load_settings("TabAway.sublime-settings")
+        enabledFileExtensions = settings.get('tabaway_file_extensions')
+
+        fileExtensions = self.getEnabledExtensions(enabledFileExtensions)
         groupsCount = self.window.num_groups()
         for i in range(0, groupsCount):
             for file in self.window.views_in_group(i):
                 fileName = file.file_name()
                 if fileName is not None:
-                    if self.getExtension(fileName) == "xbrl":
-                        print(fileName)
+                    if self.getExtension(fileName) in fileExtensions:
+                        # Do something with the files
+                        return
 
     def getExtension(self, filename):
         return filename.split('.')[-1]
+
+    def getEnabledExtensions(self, fileExtensionsSetting):
+        fileExtensions = fileExtensionsSetting.split('|')
+        return fileExtensions
 
 
 class TabAwayListener(sublime_plugin.EventListener):
